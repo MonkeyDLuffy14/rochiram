26ba5bd7885f030edf3691fc789c56f2a6fcec52
Shreedhar P <106379546+MonkeyDLuffy14@users.noreply.github.com>
2024-03-10 23:50:52 +0530

    Add files via upload

---
+++ lib/screens/notice_screen/notice_screen.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class NoticeScreen extends StatelessWidget {
+  const NoticeScreen({Key? key}) : super(key: key);
+  static String routeName = 'NoticeScreen';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text('Notice Panel'),
+        backgroundColor: Colors.blue,
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              child: Text(
+                'Important Announcement !!!',
+                style: TextStyle(fontSize: 24, color: Colors.white),
+              ),
+              padding: EdgeInsets.symmetric(vertical: 20, horizontal: 12),
+              color: Colors.red,
+            ),
+            SizedBox(height: 20), // Adding space between text and image
+            Image(
+              image: AssetImage('assets/images/notice.jpg'),
+              width: 350,
+              height: 350,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
